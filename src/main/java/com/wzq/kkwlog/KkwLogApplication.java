efe96f1c6fa2222e24612cee98cba7b26b5dd686
w237354468 <w237354468@hotmail.com>
2022-12-24 16:50:33 +0800

    init project

---
+++ src/main/java/com/wzq/kkwlog/KkwLogApplication.java
@@ -0,0 +1,13 @@
+package com.wzq.kkwlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkwLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkwLogApplication.class, args);
+	}
+
+}
